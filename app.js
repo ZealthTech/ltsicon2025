6881809090c1fb5526a8d04cff2129d52696dd74
ezee-zealth <ezee.zealth@gmail.com>
2025-10-31 18:26:30 +0530

    created utility table

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use("/uploads/abstractFiles", express.static("uploads/abstractFiles"));
 app.use("/uploads/qrcodeImages", express.static("uploads/qrcodeImages"));
 app.use("/uploads/albumImages", express.static("uploads/albumImages"));
 app.use("/uploads/thumbnails", express.static("uploads/thumbnails"));
+app.use("/uploads/generalInfo", express.static("uploads/generalInfo"));
 app.use(
   "/uploads/screenShots",
   express.static("uploads/screenShots")
